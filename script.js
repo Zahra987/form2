3cf05cffa1ca2669c2ad5f47bad80571e5c6c270
zahra987 <zhr.ramezanpour@gmail.com>
2023-03-04 23:06:29 +0330

    select inputs container and add onclick for signin link

---
+++ script.js
@@ -0,0 +1,19 @@
+var title= document.getElementById('title');
+var signin= document.getElementById('signin');
+var btn= document.getElementById('btn');
+var username= document.getElementById('username');
+var age= document.getElementById('age');
+var email= document.getElementById('email');
+var password= document.getElementById('password');
+var confirm= document.getElementById('confirm');
+
+signin.onclick=function(){
+  if (signin.innerHTML==='sign in') {
+    signin.innerHTML='sign up';
+
+  }else{
+    signin.innerHTML='sign in';
+    
+  }
+  
+}
